0a924d78f8019e72dc9fe963c4e6afcd508bdf2d
Iqbal <ahmadiqbalramadhan6@gmail.com>
2026-01-21 13:25:08 +0700

    perbaikan design kelola data master

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -17,8 +17,8 @@ function Layout({ navItems }) {
 
   // Tentukan apakah user adalah operator
   const isOperator =
-    userRole === "operatorEselon1" || 
-    userRole === "operatorEselon2" || 
+    userRole === "operatorEselon1" ||
+    userRole === "operatorEselon2" ||
     userRole === "operatorUPT";
 
   // Tentukan teks eselon yang ditampilkan
@@ -26,10 +26,10 @@ function Layout({ navItems }) {
     userRole === "operatorEselon1"
       ? namaEselon1
       : userRole === "operatorEselon2"
-      ? namaEselon2
-      : userRole === "operatorUPT"
-      ? namaUPT
-      : "";
+        ? namaEselon2
+        : userRole === "operatorUPT"
+          ? namaUPT
+          : "";
 
   // Tentukan profile path berdasarkan role (hanya untuk operator)
   const getProfilePath = () => {
@@ -110,10 +110,10 @@ function Layout({ navItems }) {
             strokeLinecap="round"
             strokeLinejoin="round"
           >
-            <rect x="3" y="3" width="7" height="7"></rect>
-            <rect x="14" y="3" width="7" height="7"></rect>
-            <rect x="14" y="14" width="7" height="7"></rect>
-            <rect x="3" y="14" width="7" height="7"></rect>
+            <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path>
+            <polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline>
+            <line x1="12" y1="22.08" x2="12" y2="12"></line>
+            <circle cx="12" cy="12" r="1.6"></circle>
           </svg>
         );
       case "laporan":
